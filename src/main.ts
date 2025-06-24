cd1b82a78aa51069c5371f7772f271b3986418e6
Mateus Borges <borgesm.work@gmail.com>
2025-06-24 18:49:18 -0300

    feat: tranform interceptor

--- src/main.ts
+++ src/main.ts
@@ -2,11 +2,13 @@ import 'reflect-metadata';
 
 import { AppModule } from './app.module';
 import { NestFactory } from '@nestjs/core';
+import { TransformInterceptor } from './transform.interceptor';
 import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
+  app.useGlobalInterceptors(new TransformInterceptor());
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
